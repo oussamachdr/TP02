52fd92403330c4e464ca0272e649a2a3c6e1fdf7
oussama <oussamachdr@gamil.com>
2024-03-21 10:10:52 +0100

    Adding Test of Calculatrice class

--- src/test/java/org/example/CalculatriceTest.java
+++ src/test/java/org/example/CalculatriceTest.java
@@ -13,20 +13,22 @@ public class CalculatriceTest {
     private Calculatrice calculatrice;
     @Test
     public void testAdditionner() {
-    // Définition du comportement de la méthode "additionner"
+        // Définition du comportement de la méthode "additionner"
         when(calculatrice.additionner(2, 3)).thenReturn(5);
-    //TODO : Appel de la méthode à tester
-    int Result = calculatrice.additionner(2,3);
-    //TODO : Vérification du résultat
-    Assertions.assertEquals(5,Result);
-    //TODO : Vérification que la méthode "additionner" a été appelée
-    // avec les arguments 2 et 3, utiliser la directive « verify »
+        //TODO : Appel de la méthode à tester
+        int Result = calculatrice.additionner(2,3);
+        //TODO : Vérification du résultat
+        Assertions.assertEquals(5,Result);
+        //TODO : Vérification que la méthode "additionner" a été appelée
+        // avec les arguments 2 et 3, utiliser la directive « verify »
+        verify(calculatrice).additionner(2,3);
+        //TODO : Vérification qu'aucune autre méthode n'a été appelée sur
+        //l'objet après l'appel de la méthode "additionner", utiliser la
+        // méthode « verifyNoMoreInteractions
+        verifyNoMoreInteractions(calculatrice);
+        // TODO : Vérification de l'état de l'objet après l'appel de la
+        //méthode "additionner", penser à utiliser la méthode
+        //« getState() » de la directive « verify » : // exemple :verify(objet).getState()
 
-    //TODO : Vérification qu'aucune autre méthode n'a été appelée sur
-    //l'objet après l'appel de la méthode "additionner", utiliser la
-    // méthode « verifyNoMoreInteractions »
-    // TODO : Vérification de l'état de l'objet après l'appel de la
-    //méthode "additionner", penser à utiliser la méthode
-    //« getState() » de la directive « verify » : // exemple :verify(objet).getState()
     }
 }
